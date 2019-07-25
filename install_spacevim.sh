a6b786540490c84194d73f3d6b55a6960f5e06b0
libingchen <libingchen@libingchen.me>
2019-07-25 09:19:00 +0800

    fix

--- install_spacevim.sh
+++ install_spacevim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source utils/shutils.sh
 
-#CHECK sudo yum install ncurses-devel -y
-#CHECK wget https://github.com/vim/vim/archive/master.zip 
-#CHECK unzip master.zip 
-#CHECK cd vim-master/src/ 
-#CHECK ./configure --enable-multibyte --enable-pythoninterp=yes
-#CHECK make && sudo make install 
-#vim --version
+CHECK sudo yum install ncurses-devel -y
+CHECK wget https://github.com/vim/vim/archive/master.zip 
+CHECK unzip master.zip 
+CHECK cd vim-master/src/ 
+CHECK ./configure --enable-multibyte --enable-pythoninterp=yes
+CHECK make && sudo make install 
+vim --version
 CHECK source ~/.zshrc
 CHECK curl -sLf https://spacevim.org/install.sh | bash
