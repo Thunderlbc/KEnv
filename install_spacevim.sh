277b192119bc4671c0ca599255bb2ce8ed7ffb4a
libingchen <libingchen@365du.com>
2020-04-13 16:44:19 +0800

    update

--- install_spacevim.sh
+++ install_spacevim.sh
@@ -2,11 +2,15 @@
 source utils/shutils.sh
 
 CHECK sudo yum install ncurses-devel -y
-CHECK wget https://github.com/vim/vim/archive/master.zip 
+if [ ! -f master.zip]; then
+    CHECK wget https://github.com/vim/vim/archive/master.zip 
+else
+    echo "master.zip exists skipping wget"
+fi
 CHECK unzip master.zip 
 CHECK cd vim-master/src/ 
 CHECK ./configure --enable-multibyte --enable-pythoninterp=yes
-CHECK make && sudo make install 
+CHECK make -j4 && sudo make install 
 vim --version
 CHECK source ~/.zshrc
 CHECK curl -sLf https://spacevim.org/install.sh | bash
